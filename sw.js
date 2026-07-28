c1bf7de07347410dd480fa545fda27a783e180ee
CyberSaroel <sarobagmanyan@gmail.com>
2026-07-28 04:43:06 +0400

    update version to 1.6.0 in index.html, version.js, and sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1.5.3"; // это число будет менять tools/bumpVersion.js
+const VERSION = "1.6.0"; // это число будет менять tools/bumpVersion.js
 const CACHE = "royal-socio-cats-" + VERSION;
 
 // Ставим новую версию воркера сразу, не ждём закрытия вкладок
